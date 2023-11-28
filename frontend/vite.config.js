9b10bf7ee241e6aa1ef3bc6271c8072f92b6cdea
sharbas <mohammedsharbas32@gmail.com>
2023-11-28 20:46:50 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-port:3000,
-proxy:{
-  '/api':{
-    target:'http://localhost:5000',
-    changeOrigin:true
-  }
-}
-  }
-})
+
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [
+    Checker({ typescript: true }), // Adjust options as needed
+  ],
+});
