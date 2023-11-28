b02d4d814cf4312a6316a9978622a777a34e9767
sharbas <mohammedsharbas32@gmail.com>
2023-11-28 18:38:02 +0530

    14th commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://mohammedsharbas33:sharbas12345678@ac-ohqgxsc-shard-00-00.6fhmvos.mongodb.net:27017,ac-ohqgxsc-shard-00-01.6fhmvos.mongodb.net:27017,ac-ohqgxsc-shard-00-02.6fhmvos.mongodb.net:27017/?ssl=true&replicaSet=atlas-cz2k8d-shard-0&authSource=admin&retryWrites=true&w=majority"
-    , {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // useCreateIndex: true, 
