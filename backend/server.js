1bdc9ae58b4445c9462422b6959295c11eb8c96f
sharbas <mohammedsharbas32@gmail.com>
2023-11-30 11:49:10 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.static('backend/public'))
-app.use('/images',express.static('/images'))
+// app.use('/images',express.static('/images'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
