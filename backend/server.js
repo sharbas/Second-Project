6d5db24b6b3b19dc57b2f8e8e21589ab73514db5
sharbas <mohammedsharbas32@gmail.com>
2023-11-30 11:39:38 +0530

    change in images

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.static('backend/public'))
+app.use('/images',express.static('backend/public/images'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
